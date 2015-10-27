d0131bbfc1ffebb9115f567fd2c4637acf577e65
woutergoku <wouter1993@hotmail.com>
2015-10-27 13:30:22 +0100

    Uitlog optie

--- Notflix/WebContent/Scripts/script.js
+++ Notflix/WebContent/Scripts/script.js
@@ -1,6 +1,22 @@
 $(document).ready(function() {
-	localStorage.removeItem("Token");
 	console.log("[ready] - enter");
+	
+	if(typeof(Storage) !== "undefined") {
+		var token = localStorage.getItem("Token");
+		if(token != null) {
+			console.log("[Storage True] - enter");
+			$("#form").append("<button id='btnLogout' type='button' class='btn btn-danger'>Log Out</button><p id='welcomeMessage'>Welkom</p>");
+		} else {
+			console.log("[Storage False] - enter");
+			$("#form").append("<form class='navbar-form navbar-right' id='loginForm' onsubmit='login(); return false;' role='login' method='post'><div class='form-group'><input type='text' class='form-control' name='nickname' placeholder='Nickname'></div><div class='form-group'><input type='password' class='form-control' name='password' placeholder='Password'></div><button type='submit' id='btnLogin' class='btn btn-success'>Login</button></form><a id='registreer' href='registreer.html'>registreer</a>");
+		}
+	} else {
+	}
+	
+	$("#btnLogout").click(function() {
+		console.log("[Logout Click] - enter");
+		localStorage.removeItem("Token");
+	});
 });
 
 function login() { 
@@ -20,7 +36,7 @@ function login() {
 		}
 	});
 	
-	return false;
+	//return false;
 }
 
 function registreer() {
